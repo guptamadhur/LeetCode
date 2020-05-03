a839f244cab2df7d3f0d5c73646c1ea5ac60bacc
MADHUR GUPTA <guptamadhur96@gmail.com>
2020-05-04 03:21:57 +0530

    Frist Bad Version

---
+++ src/web/app/madhurgupta/challenge/may/Week1/FirstBadVersion.java
@@ -0,0 +1,10 @@
+package web.app.madhurgupta.challenge.may.Week1;
+/*
+# Author: Madhur Gupta
+# Github: github.com/guptamadhur
+# Project: LeetCode
+# Created on: 04-05-2020 02:39
+*/
+
+public class FirstBadVersion {
+}
