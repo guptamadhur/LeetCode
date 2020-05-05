692a9a28f23a33468fee6fc5eeec08c7cad298d0
MADHUR GUPTA <guptamadhur96@gmail.com>
2020-05-06 02:28:59 +0530

    First_Unique_Character_in_a_String

--- src/web/app/madhurgupta/challenge/may/Week1/First_Unique_Character_in_a_String.java
+++ src/web/app/madhurgupta/challenge/may/Week1/First_Unique_Character_in_a_String.java
@@ -13,28 +13,52 @@ s = "loveleetcode",
 return 2
 */
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class First_Unique_Character_in_a_String {
     public static void main(String[] args) {
         First_Unique_Character_in_a_String ob = new First_Unique_Character_in_a_String();
-        System.out.println(ob.firstUniqChar(""));
+        System.out.println(ob.firstUniqChar("loveleetcode"));
+        System.out.println(ob.firstUniqueChar("loveleetcode"));
     }
+
     public int firstUniqChar(String s) {
-        if(s.isEmpty())
+        if (s.isEmpty())
             return -1;
-        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
-        for(char ch :s.toCharArray()){
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (char ch : s.toCharArray()) {
             Integer v = map.get(ch);
-            if(v != null){
-                map.put(ch,new Integer(v+1));
-            }else{
-                map.put(ch,1);
+            if (v != null) {
+                map.put(ch, new Integer(v + 1));
+            } else {
+                map.put(ch, 1);
             }
         }
         Map.Entry<Character, Integer> ss = map.entrySet().stream().min(Comparator.comparingInt(Map.Entry::getValue)).get();
         return ss.getValue() > 1 ? -1 : s.indexOf(ss.getKey());
+    }
+
+    public int firstUniqueChar(String s) {
+        if (s.isEmpty())
+            return -1;
+        int res=-1;
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (char ch : s.toCharArray()) {
+            Integer v = map.get(ch);
+            if (v != null) {
+                map.put(ch, new Integer(v + 1));
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        for(Map.Entry<Character,Integer> val :map.entrySet()){
+            if(val.getValue()==1){
+                res = s.indexOf(val.getKey());
+                break;
+            }
+        }
+        return res;
     }
 }
